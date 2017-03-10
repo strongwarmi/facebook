b213fbc600ba0efbbc6c90ae39392bacfb91d1a6
strongwarmi <elianasc10@hotmail.com>
2017-03-10 18:17:40 -0500

    solo frontend login

---
+++ assets/js/login.js
@@ -0,0 +1,14 @@
+window.addEventListener('load',function() {
+  var loginButton = document.getElementById('login');
+  loginButton.addEventListener('click',function() {
+    var emailInput = document.getElementById('inputemail');
+    var passwordInput = document.getElementById('inputpassword');
+    if (emailInput.value == "" ) {
+      Email.innerHTML = "* Este campo es requerido";
+    }
+    if (passwordInput.value == "") {
+      Password.innerHTML = "* Este campo es requerido";
+    }
+    
+  });
+});
